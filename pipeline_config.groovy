943e9ce1ea375fc72efc22b4e53d10918590532a
samsonl <lyndon.samson@gmail.com>
2020-07-07 15:47:29 +1000

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,31 +1,25 @@
-allow_scm_jenkinsfile = false
+allow_scm_jenkinsfile = true
 skip_default_checkout = true
 merge = true 
-//override = true 
+override = true 
 
-pipeline_template = "Jenkinsfile2"
 
 application_environments {
   global {
      merge = true
      A = "jteconfg A"
      B = "jteconfg B"
- }
+  }
 }
-
 
 keywords {
     version = "V1.00"
 }
 
-
 libraries{
   merge = true 
-  libsimple {
-      override = true
-      A = "jteconfg A from lib"
-   }
- 
+}
+
 template_methods{
 }
 
